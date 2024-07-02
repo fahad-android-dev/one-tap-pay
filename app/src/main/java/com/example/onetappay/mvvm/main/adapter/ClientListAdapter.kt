1c688e7fdbfefed2c58daa540273123f608c1306
Fahad <fahadoffice14@gmail.com>
2024-07-02 16:49:03 +0530

    - tcp server added

---
+++ app/src/main/java/com/example/onetappay/mvvm/main/adapter/ClientListAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.onetappay.mvvm.main.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.onetappay.R
+import com.example.onetappay.databinding.LvItemClientListBinding
+import com.example.onetappay.interfaces.CommonInterfaceClickEvent
+
+class ClientListAdapter(): RecyclerView.Adapter<ClientListAdapter.MyViewHolder>() {
+
+    var arrClientList: MutableList<String> = ArrayList()
+    var onClickEvent: CommonInterfaceClickEvent? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding: LvItemClientListBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.lv_item_client_list,
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val a = arrClientList[position]
+
+        holder.binding.txtId.text = "Client Id : ${a}"
+
+        holder.binding.rootLayout.setOnClickListener {
+            onClickEvent?.onItemClick("itemClicked",position)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return arrClientList.size
+    }
+
+    class MyViewHolder(var binding: LvItemClientListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(data: MutableList<String>) {
+        if (data.isNullOrEmpty()) {
+            arrClientList = ArrayList()
+        }
+        arrClientList = data
+        notifyDataSetChanged()
+    }
+
+    fun updateClients(newClients: List<String>) {
+        arrClientList.clear()
+        arrClientList.addAll(newClients)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/onetappay/mvvm/main/view/MainActivity.kt
+++ app/src/main/java/com/example/onetappay/mvvm/main/view/MainActivity.kt
@@ -1,14 +1,102 @@
 package com.example.onetappay.mvvm.main.view
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.onetappay.R
+import com.example.onetappay.databinding.ActivityMainBinding
+import com.example.onetappay.helper.TCPServer
+import com.example.onetappay.helper.WebSocketClient
+import com.example.onetappay.interfaces.MessageListener
+import com.example.onetappay.mvvm.main.adapter.ClientListAdapter
+import java.io.OutputStream
+import java.net.Socket
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MessageListener {
+    private lateinit var tcpServer: TCPServer
+    private lateinit var webSocketClient: WebSocketClient
+    private lateinit var binding: ActivityMainBinding
+    private var outStream: OutputStream? = null
+    private lateinit var socket : Socket
+    private var adapter = ClientListAdapter()
+    private  var arrListClients = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.rvClients.adapter = adapter
+
+        initializeSocket()
+
     }
+
+    private fun initializeSocket(){
+        // Initialize and start TCP server
+        tcpServer = TCPServer(8085,this)
+        Thread {
+            tcpServer.start()
+        }.start()
+
+
+
+
+        // Initialize and start WebSocket server
+        webSocketClient = WebSocketClient(8085)
+        webSocketClient.start()
+
+        /* binding.btnSend.setOnClickListener {
+             val message = binding.edtMessage.text.toString().trim()
+             if (message.isNotEmpty()) {
+                 binding.edtMessage.text.clear()
+             }
+         }*/
+    }
+
+    private fun updateClientList(clients: List<String>) {
+        runOnUiThread {
+            adapter.updateClients(clients)
+        }
+    }
+
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tcpServer.stop()
+        webSocketClient.stop()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onMessageReceived(message: String) {
+        runOnUiThread {
+            println("Received message in activity: $message")
+            //tcpServer.handleMessage(message)
+        }
+    }
+
+    override fun onClientConnected(clientSocket: Socket?) {
+        Thread {
+            try {
+                outStream = clientSocket?.getOutputStream()
+                if (clientSocket != null) {
+                    socket = clientSocket
+                    runOnUiThread {
+                        tcpServer.observeClientList().observe(this) { clients ->
+                            println("here is client list ${clients}")
+                            arrListClients.addAll(clients)
+
+                            updateClientList(clients)
+                        }
+                    }
+                }
+                println("Connected to server")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
 }
